0c79c763e7b4bdc0306b9c2c98b462a41ec34c69
Adriatticus <4CorderJ@gmail.com>
2025-03-06 08:02:55 -0500

    First try FizzBuzz app

---
+++ FizzBuzz/src/FizzBuzzApp.java
@@ -0,0 +1,31 @@
+
+public class FizzBuzzApp {
+
+	public static void main(String[] args) {
+		System.out.println("Fizzy pop buz yum yum\n");
+		
+		System.out.println("Welcome to the FizzBuzz App\n");
+		
+		//list<int> i = new list<int>()
+		int i = 0;
+		for (i = 0; i < 16; i++) {
+				if (i/3 == 0) {
+					System.out.println("Fizz");
+				}
+				else if (i/5 == 0) {
+					System.out.println("Buzz");
+				}
+				else if (i/3 == 0 && i/5 == 0) {
+					System.out.println("FizzBuzz");
+				}
+				else {
+					System.out.println(i);
+				}
+			}
+		}
+
+		
+		System.out.println("\nChug chug yum yum fizzy yummy buzz buzz");
+	}
+
+}
