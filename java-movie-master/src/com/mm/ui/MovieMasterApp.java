8bb6f229de7afc60859e2bb9b441435e90872e8b
adrian-johnson <4corderoj@gmail.com>
2025-03-11 08:14:11 -0400

    movie master and guessing game progress

---
+++ java-movie-master/src/com/mm/ui/MovieMasterApp.java
@@ -0,0 +1,81 @@
+package com.mm.ui;
+
+import com.mm.db.DAO;
+import com.mm.model.Movie;
+import com.mm.db.MovieFileDAO;
+
+import Util.MyConsole;
+
+public class MovieMasterApp {
+	
+	private static DAO<Movie> movieDAO = new MovieFileDAO();
+
+	public static void main(String[] args) {
+		
+		
+		MyConsole.printHeader("Welcome to the MovieMaster App");
+		
+		String command = "";
+		while (!command.equalsIgnoreCase("exit")) {
+			displayMenu();
+			command = MyConsole.promptString("Enter command: ");
+			switch (command) {
+			case "all":
+				displayAllMovies();
+				break;
+			case "add":
+				AddMovie();
+				break;
+			case "del":
+				deleteMovie();
+				break;
+			case "exit":
+				break;
+			default:
+				MyConsole.printL("Invalid Option. Try again.");
+				break;	
+			
+		}
+		
+		
+		
+		
+		MyConsole.printL("\nBye");
+		}
+	}
+	
+	private static void displayAllMovies() {
+		MyConsole.printHeader("Display Movies:", "+");
+		//movieDAO.getAll().forEach(Movie::toString);
+		movieDAO.getAll().forEach(movie -> MyConsole.printL(movie.toString()));
+	}
+
+	private static void AddMovie() {
+		MyConsole.printHeader("Add Movie:", "+");
+		int movieId = MyConsole.promptInt("Id: ");
+		String title = MyConsole.promptString("Title: ");
+		int year = MyConsole.promptInt("Year: ");
+		String rating = MyConsole.promptString("Rating: ");
+		String director = MyConsole.promptString("Director: ");
+		movieDAO.add(new Movie(movieId, title, year, rating, director));
+		MyConsole.printL("Movie added.");
+	}
+
+	private static void deleteMovie() {
+		MyConsole.printHeader("Delete Movie:", "+");	
+		MyConsole.printHeader("Delete Movie:", "+");
+		int id = MyConsole.promptInt("Movie ID: ");
+		movieDAO.delete(id);
+		MyConsole.printL("Movie deleted.");
+		
+	}
+
+	private static void displayMenu() {
+		MyConsole.printL("Menue");
+		MyConsole.printL("==================");
+		MyConsole.printL("all - Get All Movie");
+		MyConsole.printL("add - Add a Movie");
+		MyConsole.printL("del - Delete a Movie");
+		MyConsole.printL("exit");
+	}
+}
--- java-project-5-3-guessing-game/src/GuessingGameApp.java
+++ java-project-5-3-guessing-game/src/GuessingGameApp.java
@@ -12,9 +12,42 @@ public class GuessingGameApp {
 		
 		String choice = "y";
 		while (choice == "y") {
+			int min = 1;
+			int max = 100;
+			int range = ((max-min) +1);
+			int randoNum = (int) (Math.random()*range) +1;
+			MyConsole.printL(Integer.toString(randoNum));
 			System.out.println("I'm thinking of a number from 1 to 100.\nTry to guess it.");
-			int randomInt = (int) (Math.random()* 100)+1;
-			MyConsole.promtIntInRange("Enter a number: " , 1, 100);
+			//int randomInt = (int) (Math.random()* 100)+1;
+			//MyConsole.printL(Integer.toString(randomInt));
+			int guess = 0;
+			int count = 0;
+			int dif = (guess - randoNum);
+				while (guess != randoNum) {
+					guess = MyConsole.promtIntInRange("Enter a number: " , 1, 100);
+					count++;
+					MyConsole.printL(Integer.toString(count));
+					if (dif == 0) {
+						MyConsole.printL("Make a winner method");
+						continue;
+					}
+					if (dif > -10) {
+						MyConsole.printL("Not nearly enough! That's way too low pal!");
+						continue;
+					}
+					if (dif > 10) {
+						MyConsole.printL("Whoa relax! That's way too high buddy!");
+						continue;
+					}
+					if (dif < -10) {
+						MyConsole.printL("Close! But too low yet!");
+						continue;
+					}
+					if (dif < 10) {
+						MyConsole.printL("Almost, however, your guess is too high. Try again.");
+						continue;
+					}
+				}
 
 		System.out.println("Welcome to the Guessing Game App");
 		}
