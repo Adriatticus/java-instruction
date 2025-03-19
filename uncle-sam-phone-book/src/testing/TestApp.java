3f92127d77e0898c64fbfac3e497258ba9a67cf3
adrian-johnson <4corderoj@gmail.com>
2025-03-19 16:35:35 -0400

    chapter 16 lamda strings and streams, working on uncle sams phone book

---
+++ uncle-sam-phone-book/src/testing/TestApp.java
@@ -0,0 +1,18 @@
+package testing;
+
+import util.MyConsole;
+
+public class TestApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "anagram";
+		char[] charArrayA = str.toCharArray();
+		String giveString = charArrayA.toString();
+	
+		
+		
+		MyConsole.printL(giveString);
+	}
+
+}
